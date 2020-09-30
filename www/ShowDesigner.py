82b8e0821f2f343d31d11d3f62ddb85b03bf5db5
James Andrews <james@anagramic.co.uk>
2020-09-30 20:07:15 +0100

    example routes

---
+++ www/ShowDesigner.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    answer = 4 + 5
+    return f'<h1>Answer is {answer}</h1>'
+    
+@app.route('/microphone/<number>')
+def microphone(number):
+    return f'<h1>This is microphone {number}</h1>'
