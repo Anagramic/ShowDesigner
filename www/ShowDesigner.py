30739c180b017dd250d03245b6178d3c74edd57d
James Andrews <james@anagramic.co.uk>
2020-10-01 11:04:55 +0100

    adding a template

--- www/ShowDesigner.py
+++ www/ShowDesigner.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     answer = 4 + 6
-    return f'<h1>Answer is {answer}</h1>'
+    return render_template('index.html')
 
 @app.route('/microphone/<number>')
 def microphone(number):
-    return f'<h1>This is microphone {number}</h1>'
+    return render_template('number.html',number=number)
