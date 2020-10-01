143b73137f327d5ff3ff4b7a50ceb3d94f0fbcfe
James Andrews <james@anagramic.co.uk>
2020-10-01 11:37:25 +0100

    playing around with jinja

--- www/ShowDesigner.py
+++ www/ShowDesigner.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from jinja2 import Template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     answer = 4 + 6
+    
     return render_template('index.html',answer=answer)
 
-@app.route('/microphone/<number>')
-def microphone(number):
-    return render_template('number.html',number=number)
+#@app.route('/microphone/<number>')
+#def microphone(number):
+#    return render_template('number.html',number=number)
