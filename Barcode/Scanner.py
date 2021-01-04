ec60f70204ca2614b5a3c5bd25e41766d1b9c240
James Andrews <james@anagramic.co.uk>
2021-01-04 13:52:33 +0000

    barcode to binary added

--- Barcode/Scanner.py
+++ Barcode/Scanner.py
@@ -1,31 +1,47 @@
 from PIL import Image
 import numpy as np
 
-def conv(li,d):   
+def bin_to_dec(binary):
+    i=0
+    length = len(str(binary)) -1
+    dec = 0
+    while binary != '':
+        if binary[length] == 1:
+            dec += 2^i
+            del binary[length]
+        
+        i+=1
+
+    return(dec)
+
+
+def conv(li,digits):   
     num = []
     
-    for i in range(d):
-        start = li[int((len(li)/d)*(i-1))]
-        end = li[int((len(li)/d)*i)]
+    for i in range(digits):
+#        start = li[int((len(li)/d)*(i-1))]
+#        end = li[int((len(li)/d)*i)]
+        start = int((i/digits) * len(li))
+        end = int(((i+1)/digits) * len(li))
+        
         ones =0
         zeros =0
         
         for pxl in range(start,end):
-            print(pxl)
             
-            if pxl == 0:
+            if li[pxl] == 0:
                 zeros+=1
     
             else:
                 ones+=1
         
         if zeros>ones:
-            num.append(0)
+            num.append('0')
         
         else:
-            num.append(1)
-    print(num)
-    #return(str(''.join(num)))       
+            num.append('1')
+    
+    return(int(''.join(num)))       
 
 
 image=Image.open("1.jpg")
@@ -42,15 +58,15 @@ for i in range(width):
 for i in range(len(line)):
     
     if int(line[i]) > 127:
-        line[i] = 1
+        line[i] = 0
     
     else:
-        line[i] = 0
+        line[i] = 1
 
 while True: 
     i=line[0]
     
-    if i ==1:
+    if i ==0:
         del line[0]
     
     else:
@@ -59,14 +75,14 @@ while True:
 while True: 
     i=line[len(line)-1]
     
-    if i ==1:
+    if i ==0:
         del line[len(line)-1]
     
     else:
         break
 #print(line)
 
-print(conv(line,47))
+print(bin_to_dec(conv(line,47)))
 
 
 
